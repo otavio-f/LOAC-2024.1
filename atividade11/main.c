c63697b4a30ce459388f2a6835f13a62169f7c7e
Otávio Ferreira <38397566+otavio-f@users.noreply.github.com>
2024-05-21 11:05:09 -0300

    Add files via upload

--- atividade11/main.c
+++ atividade11/main.c
@@ -4,9 +4,45 @@ void
 select_segment(int);
 
 void
-light_up(int);
+set_pin(int, int);
 
-int lookup_table[] = {0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110, 0b01101101, 0b01111101, 0b00000111, 0b01111111, 0b01100111};
+void
+display_digit(int);
+
+void
+sleep(int);
+
+void
+clear_display();
+
+void
+zero();
+
+void
+two();
+
+void
+three();
+
+void
+four();
+
+void
+five();
+
+void
+six();
+
+void
+seven();
+
+void
+eight();
+
+void
+nine();
+
+/*const int digits[] = {0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110, 0b01101101, 0b01111101, 0b00000111, 0b01111111, 0b01100111};*/
 
 void
 main()
@@ -14,65 +50,225 @@ main()
 	int i;
 	for(i=0; i<=99; i++)
 	{
-		P0 = lookup_table[i/10];
+		P0 = 0; /*limpa tudo imediato*/
 		select_segment(0);
-		/*light_up(i/10);*/
-		P0 = lookup_table[i%10];
+		display_digit(i/10); /*00, 10, 20, ...*/
+		clear_display();
 		select_segment(1);
-		/*light_up(i%10);*/
-
-		P1 = 0XFF;
-		P0 = 0;
+		display_digit(i%10); /*1, 2, 3...*/
+		sleep(10);
 	}
 }
 
+
+void
+display_digit(int d)
+{
+	if(d==0)
+		zero();
+	else if(d==1)
+		one();
+	else if(d==2)
+		two();
+	else if(d==3)
+		three();
+	else if(d==4)
+		four();
+	else if(d==5)
+		five();
+	else if(d==6)
+		six();
+	else if(d==7)
+		seven();
+	else if(d==8)
+		eight();
+	else if(d==9)
+		nine();
+}
 
 void
 select_segment(int s)
 {
 	if(s) /*s==1*/
 	{
-		P1_0 = 1;
-		P1_1 = 0;
+		P1 = 0b00000001;
 	}
 	else
 	{
-		P1_0 = 0;
-		P1_1 = 1;
+		P1 = 0b00000010;
 	}
 }
 
 void
-light_up(int i)
+set_pin(int pin, int value)
 {
-	if(i==0)
-		P0 = 0b00111111;
-		return;
-	if(i==1)
-		P0 = 0b00000110;
-		return;
-	if(i==2)
-		P0 = 0b01011011;
-		return;
-	if(i==3)
-		P0 = 0b01001111;
-		return;
-	if(i==4)
-		P0 = 0b01100110;
-		return;
-	if(i==5)
-		P0 = 0b01101101;
-		return;
-	if(i==6)
-		P0 = 0b01111101;
-		return;
-	if(i==7)
-		P0 = 0b00000111;
-		return;
-	if(i==8)
-		P0 = 0b01111111;
-		return;
-	if(i==9)
-		P0 = 0b01100111;
-		return;
+	if(pin==0)
+		P0_0 = value;
+	else if(pin==1)
+		P0_1 = value;
+	else if(pin==2)
+		P0_2 = value;
+	else if(pin==3)
+		P0_3 = value;
+	else if(pin==4)
+		P0_4 = value;
+	else if(pin==5)
+		P0_5 = value;
+	else if(pin==6)
+		P0_6 = value;
+	else if(pin==7)
+		P0_7 = value;
 }
+
+void
+sleep(int time)
+{
+	while(time--);
+}
+
+void
+clear_display()
+{
+	P0_0 = 0;
+	P0_1 = 0;
+	P0_2 = 0;
+	P0_3 = 0;
+	P0_4 = 0;
+	P0_5 = 0;
+	P0_6 = 0;
+	P0_7 = 0;
+}
+
+void
+zero()
+{
+	P0_0 = 1;
+	P0_1 = 1;
+	P0_2 = 1;
+	P0_3 = 1;
+	P0_4 = 1;
+	P0_5 = 1;
+	P0_6 = 0;
+	P0_7 = 0;
+}
+
+
+void
+one()
+{
+	P0_0 = 0;
+	P0_1 = 1;
+	P0_2 = 1;
+	P0_3 = 0;
+	P0_4 = 0;
+	P0_5 = 0;
+	P0_6 = 0;
+	P0_7 = 0;
+}
+
+
+void
+two() /*0b01011011*/
+{
+	P0_0 = 1;
+	P0_1 = 1;
+	P0_2 = 0;
+	P0_3 = 1;
+	P0_4 = 1;
+	P0_5 = 0;
+	P0_6 = 1;
+	P0_7 = 0;
+}
+
+void
+three() /*0b01001111*/
+{
+	P0_0 = 1;
+	P0_1 = 1;
+	P0_2 = 1;
+	P0_3 = 1;
+	P0_4 = 0;
+	P0_5 = 0;
+	P0_6 = 1;
+	P0_7 = 0;
+}
+
+void
+four() /*0b01100110*/
+{
+	P0_0 = 0;
+	P0_1 = 1;
+	P0_2 = 1;
+	P0_3 = 0;
+	P0_4 = 0;
+	P0_5 = 1;
+	P0_6 = 1;
+	P0_7 = 0;
+}
+
+void
+five() /*0b01101101*/
+{
+	P0_0 = 1;
+	P0_1 = 0;
+	P0_2 = 1;
+	P0_3 = 1;
+	P0_4 = 0;
+	P0_5 = 1;
+	P0_6 = 1;
+	P0_7 = 0;
+}
+
+
+void
+six() /*0b01111101*/
+{
+	P0_0 = 1;
+	P0_1 = 0;
+	P0_2 = 1;
+	P0_3 = 1;
+	P0_4 = 1;
+	P0_5 = 1;
+	P0_6 = 1;
+	P0_7 = 0;
+}
+
+void
+seven() /*0b00000111*/
+{
+	P0_0 = 1;
+	P0_1 = 1;
+	P0_2 = 1;
+	P0_3 = 0;
+	P0_4 = 0;
+	P0_5 = 0;
+	P0_6 = 0;
+	P0_7 = 0;
+}
+
+void
+eight() /*0b01111111*/
+{
+	P0_0 = 1;
+	P0_1 = 1;
+	P0_2 = 1;
+	P0_3 = 1;
+	P0_4 = 1;
+	P0_5 = 1;
+	P0_6 = 1;
+	P0_7 = 0;
+}
+
+void
+nine() /*0b01100111*/
+{
+	P0_0 = 1;
+	P0_1 = 1;
+	P0_2 = 1;
+	P0_3 = 0;
+	P0_4 = 0;
+	P0_5 = 1;
+	P0_6 = 1;
+	P0_7 = 0;
+}
+
